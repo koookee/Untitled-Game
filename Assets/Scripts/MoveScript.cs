31b493b2b90ad5a6deeb7cba40cde19a22675aed
koookee <55768904+koookee@users.noreply.github.com>
2020-08-19 04:29:47 -0400

    Made player shoot bullets

---
+++ Assets/Scripts/MoveScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScript : MonoBehaviour
+{
+    //Edit this depending on which direction you want an object to move in
+    private Vector3 moveDirection = new Vector3(0, 0, 1);
+    private int speed = 5;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(moveDirection * Time.deltaTime * speed);
+    }
+}
---
+++ Assets/Scripts/SpawnManagerScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerScript : MonoBehaviour
+{
+    public GameObject[] prefabs;
+    public PlayerController PlayerControllerScript;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bulletSpawner();
+    }
+    public void bulletSpawner()
+    {
+        //Spawns bullets from the player's side
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 bulletPos = new Vector3(PlayerControllerScript.transform.position.x + 1, PlayerControllerScript.transform.position.y, PlayerControllerScript.transform.position.z);
+            Instantiate(prefabs[0], bulletPos, prefabs[0].transform.rotation);
+        }
+    }
+}
