eb772e4cc6b7d35f29663d144dd94e6cbfdd9012
koookee <55768904+koookee@users.noreply.github.com>
2020-09-11 07:24:11 -0400

    Player camera now rotates up and down. Added a boundry that the player camera can rotate in

---
+++ Assets/Scripts/RotateScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateScript : MonoBehaviour
+{
+    private int rotateSpeed = 5 * 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //320 35
+        float verticalInput = Input.GetAxis("Mouse Y");
+        float rotation = transform.rotation.eulerAngles.x;
+        if (rotation >= 340 || rotation <= 35)
+        {
+            transform.Rotate(Vector3.left * Time.deltaTime * verticalInput * rotateSpeed);
+        }
+        else if (rotation > 35 && rotation < 150)
+        {
+            transform.eulerAngles = new Vector3(35, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
+        }
+        else if (rotation < 340 && rotation > 250)
+        {
+            transform.eulerAngles = new Vector3(340, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
+        }
+        Debug.Log(transform.rotation.eulerAngles.x);
+
+    }
+}
